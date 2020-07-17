71c446da449910cf2739415bb69ae2e8689cb299
nabek <nabekabebe6@gmail.com>
2020-07-17 09:53:35 +0300

    binding common text editor event handlers and add scroll bar event.

--- textarea.py
+++ textarea.py
@@ -3,6 +3,30 @@ import tkinter as tk
 class TextArea(tk.Text):
 	def __init__(self, master, **kwargs):
 		super().__init__(**kwargs)
+		self.bind_events()
 		self.master = master
 		self.config(wrap=tk.WORD)
+	def bind_events(self):
+		self.bind("<Control-a>",self.select_all)
+		self.bind("<Control-c>", self.copy)
+		self.bind("<Control-v>", self.paste)
+		self.bind("<Control-x>", self.cut)
+		self.bind("<Control-y>", self.redo)
+		self.bind("<Control-z>", self.undo)
+
+	def copy(self, event=None):
+		self.event_generate("<<Control-c>>")
+	def paste(self, event=None):
+		self.event_generate("<<Control-v>>")
+	def cut(self, event=None):
+		self.event_generate("<<Control-x>>")
+	def redo(self, event=None):
+		self.event_generate("<<Control-y>>")
+		return "break"
+	def undo(self, event=None):
+		self.event_generate("<<Control-z>>")
+		return "break"
+	def select_all(self, event=None):
+		self.tag_add("sel", 1.0, tk.END)
+		return "break"
 
--- texteditor.py
+++ texteditor.py
@@ -10,11 +10,38 @@ class MainWindow(tk.Tk):
 		self.scrollbar = ttk.Scrollbar(orient="vertical",command=self.scroll_text)
 		self.text_area.configure(yscrollcommand = self.scrollbar.set)
 
+		self.line_numbers = tk.Text(self, bg="black", fg="white")
+		first_100_numbers = [str(n) for n in range(1,101)]
+		self.line_numbers.insert(1.0,"\n".join(first_100_numbers))
+		self.line_numbers.configure(state="disabled", width=3,yscrollcommand=self.scrollbar.set)
+
+		self.line_numbers.pack(side=tk.LEFT, fill=tk.Y)
 		self.scrollbar.pack(side = tk.RIGHT, fill = tk.Y)
 		self.text_area.pack(side = tk.LEFT, fill = tk.BOTH, expand = 1 )
-
+		self.bind_events()
 	def scroll_text(self, *args):
-		self.text_area.yview_moveto(args[1])
+		if len(args) > 1:
+			self.text_area.yview_moveto(args[1])
+			self.line_numbers.yview_moveto(args[1])
+		else:
+			event = args[0]
+			if event.delta:
+				move = -1 * (event.delta / 120)
+			else:
+				if event.num == 5:
+					move=1
+				else:
+					move=-1
+			self.text_area.yview_scroll(int(move), "units")
+			self.line_numbers.yview_scroll(int(move), "units")
+	def bind_events(self):
+		self.text_area.bind("<MouseWheel>", self.scroll_text)
+		self.text_area.bind("<Button-4>", self.scroll_text)
+		self.text_area.bind("<Button-5>", self.scroll_text)
+
+		self.line_numbers.bind("<MouseWheel>", lambda e: "break")
+		self.line_numbers.bind("<Button-4>", lambda e: "break")
+		self.line_numbers.bind("<Button-5>", lambda e: "break")
 
 if __name__ == '__main__':
 	MainWindow().mainloop()
